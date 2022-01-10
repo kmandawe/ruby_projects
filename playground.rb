62bea18538cd0a10ab4b77f42f2aaad908a13583
kmandawe <kdmandawe06@gmail.com>
2022-01-10 18:36:00 +0800

    Working with Strings

--- playground.rb
+++ playground.rb
@@ -7,3 +7,14 @@ def say_hello(thing_to_say)
 end
 
 say_hello "Hello World, Ruby is great!"
+
+# String concatenation
+sentence = 'My name is Kenneth'
+p sentence
+
+first_name = "Kenneth"
+last_name = "Mandawe"
+puts first_name + " " + last_name
+
+# String interpolation
+puts "My first name is #{first_name} and my last name is #{last_name}"
