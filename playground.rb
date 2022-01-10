25a5481d8c08705effeb17b9887cd3dbd68687d1
kmandawe <kdmandawe06@gmail.com>
2022-01-10 20:33:10 +0800

    Working with numbers

--- playground.rb
+++ playground.rb
@@ -18,3 +18,16 @@ puts first_name + " " + last_name
 
 # String interpolation
 puts "My first name is #{first_name} and my last name is #{last_name}"
+
+# Numbers
+puts 1 + 2
+
+x = 5
+y = 10
+puts y / x
+
+
+puts "I am a line"
+puts "-" * 20
+puts "I am a different line after a divider"
+20.times {print "-"}
